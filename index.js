7a339a15f6088b8512b58d27a4cfa226b36810fb
suvi shahoyi <sumaya.shahoyi@gmail.com>
2018-07-18 13:28:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution in this file!
 let companyName = 'scuber';
-function () { companyName = 'specialCompany' };
+function hey () { companyName = 'specialCompany' };
 let mostProfitableNeighborhood = 'Chelsea';
 let companyCeo = 'Susan Smith';
